aad3b8e3c5e7e8078d8df18800baf685833a5dbc
BARBEY Nicolas <22010493@etu-unicaen.fr>
2023-01-10 19:21:26 +0100

    fin create menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,13 +26,12 @@
                     <div>
                     @auth
                     <div class="dropdown">
-                        <button id="dropDownUser" class="btn"  data-bs-toggle="dropdown" aria-expanded="false">
+                        <button id="dropDownUser" class="btn btn-dark"  data-bs-toggle="dropdown" aria-expanded="false">
                             {{ Auth::user()->name }}
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropDownUser">
-                            <li class="dropdown-item">
-                                <a href="{{ route('logout') }}"  onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">Deconnection</a>
+                            <li class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >
+                                Deconnection
                             </li>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
