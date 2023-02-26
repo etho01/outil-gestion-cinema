0f87c84898ebe517cb5b3d4638a15b8ed0f698ff
BARBEY Nicolas <22010493@etu-unicaen.fr>
2023-02-26 22:29:51 +0100

    coorection visuel

--- resources/views/livewire/films/element/liste-film.blade.php
+++ resources/views/livewire/films/element/liste-film.blade.php
@@ -3,12 +3,12 @@
         <x-input.text name="nomFilm" label="Nom du film" placeholder="Nom du film" champLivewire="nom"/>
     </li>
     @if ($sizeNom < 4)
-        <li class="m-2">
-            Veillez entré un minimun de 4 caracteres
+        <li class="dropdown-item">
+            Veillez entrez un minimun de 4 caracteres
         </li>
     @else
         @foreach ($films as $film)
-        <li class="m-2" wire:click="refresh({{$film['id']}})" onclick="document.getElementById('idBtnModal{{$idElement}}').setAttribute('wire:click', 'changeFilm({{$film['id']}}, \'{{htmlspecialchars( $film['title'])}}\')');
+        <li class="dropdown-item" wire:click="refresh({{$film['id']}})" onclick="document.getElementById('idBtnModal{{$idElement}}').setAttribute('wire:click', 'changeFilm({{$film['id']}}, \'{{htmlspecialchars( $film['title'])}}\')');
             document.getElementById('idBtnModal{{$idElement}}').click() ">
             {{ $film['title'] }}
         </li>
